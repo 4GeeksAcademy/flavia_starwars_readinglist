39a7ee3dd6b135a7d2d1fc3b28db7493f8926d15
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-27 18:20:37 +0000

    feat: :lipstick: Navbar con scrolltrigger usando gsap

--- src/js/component/fondo.js
+++ src/js/component/fondo.js
@@ -4,7 +4,6 @@ import "../../styles/fondo.css";
 export const Fondo = () => {
     return (
         <div className="fondo">
-            <h1>hola mundo:)</h1>
         </div>
     )
 }
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger)
 
 export const Navbar = () => {
+	const [isInView, setIsInView] = useState(false);
+	useEffect(() => {
+		gsap.utils.toArray(".navbar").forEach(elem => {
+		  ScrollTrigger.create({
+			trigger: elem,
+			onToggle: ({isActive}) => {
+			  setIsInView(isActive);
+			}
+		  });
+		});
+	  }, []);
+	  
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className={`navbar mb-3 sticky-top ${isInView? "bg-primary" : "bg-success"}`} >
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
