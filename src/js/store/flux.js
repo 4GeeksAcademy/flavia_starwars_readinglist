a9d3ae843b8e364cf70c35be4460a151ff6c70e9
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-10-01 16:50:54 +0000

    feat: :sparkles: Se tiene en cuenta la paginación de la API para llamarla e ir cargando el contenido y se ha hecho un primer borrador de las imagenes, falta solucionar el caso de cuando están rotas

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,13 +1,29 @@
-const getState = ({ getStore, getActions, setStore }) => {
+export const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       planets: [],
+      url_next: "",
     },
     actions: {
       fetchPlanets: () => {
-        fetch("https://www.swapi.tech/api/Planets")
+        fetch("https://www.swapi.tech/api/planets")
           .then((response) => response.json())
-          .then((data) => setStore({ planets: data.results }))
+          .then((data) =>
+            setStore({ planets: data.results, url_next: data.next })
+          )
+          .catch((err) => console.log(err));
+      },
+      fetchMorePlanets: () => {
+        const store = getStore();
+        fetch(store.url_next)
+          .then((response) => response.json())
+          .then((data) => {
+            const newPlanets = [...store.planets, ...data.results];
+            setStore({
+              planets: newPlanets,
+              url_next: data.next,
+            });
+          })
           .catch((err) => console.log(err));
       },
     },
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,27 +1,28 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-  
-	return (
-	  <div className="rigo text-center">
-		{store.planets.map((item, index) => (
-		  <div className="text-white" key={index}>
-			{item.name}
-		  </div>
-		))}
-	  </div>
-	);
+  const { store, actions } = useContext(Context);
+
+  const loadMorePlanets = () => {
+    actions.fetchMorePlanets();
   };
-
-
-  
-  
-  
-  
-  
-  
+  return (
+    <div className="sectionsContainer">
+      <div className="planets text-center">
+        {store.planets.map((item, index) => (
+          <div className="text-white" key={index}>
+            {item.name}
+            <img
+              src={`https://starwars-visualguide.com/assets/img/planets/${item.uid}.jpg`}
+            ></img>
+          </div>
+        ))}
+      </div>
+      <button onClick={loadMorePlanets}>Cargar más</button>
+    </div>
+  );
+};
 
 export default Home;
