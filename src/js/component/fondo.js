6679c445b1a596e5debcf6910ef2580c3d64729b
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-10-01 14:30:51 +0000

    style: :lipstick: Opacidad del fondo y cambio de color del navbar

--- src/js/component/fondo.js
+++ src/js/component/fondo.js
@@ -1,9 +1,24 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/fondo.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 
 export const Fondo = () => {
-    return (
-        <div className="fondo">
-        </div>
-    )
-}
+  const [isInView, setIsInView] = useState(false);
+  useEffect(() => {
+    gsap.utils.toArray(".navbar").forEach((elem) => {
+      ScrollTrigger.create({
+        trigger: elem,
+        onToggle: ({ isActive }) => {
+          setIsInView(isActive);
+        },
+      });
+    });
+  }, []);
+  return (
+    <div
+      className={`fondo ${isInView ? "totalOpacity " : "lessOpacity"}`}
+    ></div>
+  );
+};
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,32 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 export const Navbar = () => {
-	const [isInView, setIsInView] = useState(false);
-	useEffect(() => {
-		gsap.utils.toArray(".navbar").forEach(elem => {
-		  ScrollTrigger.create({
-			trigger: elem,
-			onToggle: ({isActive}) => {
-			  setIsInView(isActive);
-			}
-		  });
-		});
-	  }, []);
-	  
-	return (
-		<nav className={`navbar mb-3 sticky-top ${isInView? "bg-primary" : "bg-success"}`} >
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  const [isInView, setIsInView] = useState(false);
+  useEffect(() => {
+    gsap.utils.toArray(".navbar").forEach((elem) => {
+      ScrollTrigger.create({
+        trigger: elem,
+        onToggle: ({ isActive }) => {
+          setIsInView(isActive);
+        },
+      });
+    });
+  }, []);
+
+  return (
+    <nav
+      className={`navbar mb-3 sticky-top ${
+        isInView ? "bg-transparent" : "bg-black"
+      }`}
+    >
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+      </Link>
+      <div className="ml-auto">
+        <Link to="/demo">
+          <button className="btn btn-primary">
+            Check the Context in action
+          </button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
