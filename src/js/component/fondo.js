6d583e3f5bd33667f10d1c597e11a54f7672da42
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-10-13 17:38:24 +0000

    al terminar el vídeo de fondo aparece la imagen establecida

--- src/js/component/fondo.js
+++ src/js/component/fondo.js
@@ -1,24 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../../styles/fondo.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
 export const Fondo = () => {
-  const [isInView, setIsInView] = useState(false);
+  const [scroll, setScroll] = useState(false);
+  const [videoEnded, setVideoEnded] = useState(false);
+  console.log(videoEnded)
   useEffect(() => {
     gsap.utils.toArray(".navbar").forEach((elem) => {
       ScrollTrigger.create({
         trigger: elem,
         onToggle: ({ isActive }) => {
-          setIsInView(isActive);
+          setScroll(isActive);
         },
       });
     });
   }, []);
+
+  const handleVideoEnded = () => {
+    console.log("Video ended");
+    setVideoEnded(true);
+  };
+
   return (
-    <div
-      className={`fondo ${isInView ? "totalOpacity " : "lessOpacity"}`}
-    ></div>
+    <div className={`fondo ${scroll ? "totalOpacity " : "lessOpacity"}`}>
+      <video
+        playsInline
+        autoPlay
+        muted
+        poster="https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhaW4lMjBibGFjayUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+        id="bgvid"
+        onEnded={handleVideoEnded}
+      >
+        <source
+          src="https://vod-bgc-eu-west-1.media.dssott.com/bgui/ps01/disney/bgui/2021/11/04/1636056809-star-wars.mp4"
+          type="video/webm"
+        ></source>
+      </video>
+      {videoEnded && (
+        <div
+          className="background-image"
+        ></div>
+      )}
+    </div>
   );
 };
